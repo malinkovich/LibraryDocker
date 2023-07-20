b81a2d8ce14add57f3a0c3f45521a557cfc165ba
malinkovich <108365870+malinkovich@users.noreply.github.com>
2023-07-20 18:30:04 +0400

    Initial commit (Author + Patent)

---
+++ src/test/java/ru/apotravnova/practice/Library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.apotravnova.practice.Library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
